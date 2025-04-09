1e9208b309f06058933f71bffe35c91ad5a617e4
Matheus Araujo <matheus.araujo@kukac.com.br>
2025-04-09 15:43:18 -0300

    feat: added some middlewares to app

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
-from typing import Union
-
+from fastapi.middleware.cors import CORSMiddleware
+from config.env import env
 from fastapi import FastAPI
+from app.orders.controller import order_controller
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        str(origin) for origin in env.app_cors_origins.split(",")
+    ],
+    allow_credentials=True,
+    allow_methods=[
+        str(method) for method in env.app_cors_methods.split(",")
+    ],
+    allow_headers=[
+        str(header) for header in env.app_cors_headers.split(",")
+    ],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+app.include_router(order_controller.router, prefix="/v1/no_injection", tags=["analyze"])
