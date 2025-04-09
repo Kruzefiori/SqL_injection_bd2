aeffb033d01f8e89695796481b597b932de74d78
Matheus Araujo <matheus.araujo@kukac.com.br>
2025-04-09 15:14:34 -0300

    feat: create config pkg

---
+++ config/env.py
@@ -0,0 +1,17 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Env():
+    app_cors_origins: str = getenv("APP_CORS_ORIGINS", "*")
+    app_cors_methods: str = getenv("APP_CORS_METHODS", "GET,POST")
+    app_cors_headers: str = getenv("APP_CORS_HEADERS", "Content-Type")
+    sql_db_uri: str = getenv(
+        "SQL_DB_URI",
+        "postgresql://postgres:postgres@localhost:5432/postgres")
+    sql_db_provider: str = getenv("SQL_DB_PROVIDER", "postgresql")
+
+
+env = Env()
