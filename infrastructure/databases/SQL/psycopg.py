c486f4757f131f36f79c5f1e80f854260b4d2e2d
Nico Silva <nixon.silva@tech4h.com.br>
2025-04-16 09:27:10 -0300

    feat: add repository methods for psycopg (with injection) conn

---
+++ infrastructure/databases/SQL/psycopg.py
@@ -0,0 +1,47 @@
+import psycopg2 as psycopg
+from psycopg import OperationalError
+from config.env import env
+
+class PsycopgSqlDB:
+    _instance = None
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(PsycopgSqlDB, cls).__new__(cls)
+            cls._instance.initialize()
+        return cls._instance
+    
+    def initialize(self):
+        try:
+            self.connection = psycopg.connect(
+                dsn=env.sql_db_uri,
+                autocommit=True
+            )
+            self.cursor = self.connection.cursor()
+        except OperationalError as e:
+            print(f"Error connecting to the database: {e}")
+            raise
+
+    def get_connection(self):
+        if self.connection.closed:
+            self.initialize()
+        return self.connection
+    
+    def get_cursor(self):
+        if self.cursor.connection.closed:
+            self.initialize()
+        return self.cursor
+    
+    def close(self):
+        if self.cursor:
+            self.cursor.close()
+        if self.connection:
+            self.connection.close()
+        self._instance = None
+
+    def get_db(self):   
+        return self._instance
+
+
+    def __del__(self):
+        self.close()
+        self._instance = None
