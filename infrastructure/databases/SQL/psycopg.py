5f1fd52754214f61cc03105457825d132f4a3d6c
Matheus Araujo <moohbr@gmail.com>
2025-04-16 20:19:14 -0300

    fix: remove autocommit to avoid dsn errors

--- infrastructure/databases/SQL/psycopg.py
+++ infrastructure/databases/SQL/psycopg.py
@@ -2,20 +2,19 @@ import psycopg2 as psycopg
 from psycopg import OperationalError
 from config.env import env
 
+
 class PsycopgSqlDB:
     _instance = None
+
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(PsycopgSqlDB, cls).__new__(cls)
             cls._instance.initialize()
         return cls._instance
-    
+
     def initialize(self):
         try:
-            self.connection = psycopg.connect(
-                dsn=env.sql_db_uri,
-                autocommit=True
-            )
+            self.connection = psycopg.connect(dsn=env.sql_db_uri)
             self.cursor = self.connection.cursor()
         except OperationalError as e:
             print(f"Error connecting to the database: {e}")
@@ -25,12 +24,12 @@ class PsycopgSqlDB:
         if self.connection.closed:
             self.initialize()
         return self.connection
-    
+
     def get_cursor(self):
         if self.cursor.connection.closed:
             self.initialize()
         return self.cursor
-    
+
     def close(self):
         if self.cursor:
             self.cursor.close()
@@ -38,9 +37,8 @@ class PsycopgSqlDB:
             self.connection.close()
         self._instance = None
 
-    def get_db(self):   
+    def get_db(self):
         return self._instance
-
 
     def __del__(self):
         self.close()
